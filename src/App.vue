158523a415c80d39852c2cf5af15e62cd9ffb3a5
Yuichiro Iijima <jimaichiro@gmail.com>
2019-12-17 23:55:11 +0900

    add header image

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <myheader></myheader>
+    <router-view/>
+    <b-button>Button</b-button>
+  </div>
+</template>
+
+<script>
+import myheader from './components/myheader'
+
+export default {
+  name: 'App',
+  components: {
+    myheader
+  }
+}
+</script>
+
+<style>
+body {
+
+}
+
+header {
+  width: 100%;
+  height: 10%;
+}
+</style>
---
+++ src/components/myheader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <header>
+      <h1>Portfolio</h1>
+    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link class="text-white px-3" to="/">home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="text-white px-3" to="/test1">test1</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="text-white px-3" to="/test2">test2</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="text-white px-3" to="/test3">test3</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+  header {
+    position: relative;
+    height: 50vh;
+    background: url("../assets/headerImg.jpg") center / cover;
+  }
+
+  h1 {
+    color: white;
+    text-align: center;
+    line-height: 50vh;
+  }
+</style>
