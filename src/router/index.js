81bd6d866ea2eecceada1b184f78cb892581d382
Yuichiro Iijima <jimaichiro@gmail.com>
2019-12-21 00:39:40 +0900

    delete unneccesary routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import test1 from '@/components/test1'
-import test2 from "@/components/test2";
-import test3 from "@/components/test3";
 
 Vue.use(Router)
 Vue.use(BootstrapVue)
@@ -14,25 +10,5 @@ import 'bootstrap-vue/dist/bootstrap-vue'
 
 export default new Router({
   routes: [
-    {
-      path: "/",
-      name: "HelloWorld",
-      component: HelloWorld
-    },
-    {
-      path: "/test1",
-      name: "test1",
-      component: test1
-    },
-    {
-      path: "/test2",
-      name: "test2",
-      component: test2
-    },
-    {
-      path: "/test3",
-      name: "test3",
-      component: test3
-    }
   ]
 });
