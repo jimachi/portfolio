abc8af11642b6508c865bbeca72a5b00e259ff70
Yuichiro Iijima <jimaichiro@gmail.com>
2019-12-21 01:35:06 +0900

    WIP: add profile text

---
+++ src/components/profile.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="profile__container">
+        <h2>Profile</h2>
+        <div>
+            <img src="../assets/tanukisan.png" class="profile__icon">
+            <p class="profile__text">
+                はじめまして。飯島雄一郎と申します。<br>
+                2017年4月から社会人になり、社会人になってから今に至るまでずっとIT業界で働いております。<br>
+                現在は、Webフロントエンジニアとして働いてます。
+            </p>
+        </div>
+    </div>
+    
+</template>
+
+<style scoped>
+.profile__container {
+    display: block;
+    margin: 10px;
+    width: 80%;
+    height: 500px;
+    margin-left: auto;
+    margin-right: auto;
+    background: red;
+}
+
+h2 {
+    text-align: center;
+}
+
+.profile__icon {
+    float: left;
+    border-radius: 50%;
+}
+
+.profile__text {
+    float: left;
+}
+</style>
